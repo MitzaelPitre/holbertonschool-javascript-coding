b9fbe3bb17738929a1b88bbe5ce6adec754dcf8e
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-05-31 13:38:39 -0400

    Coding

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,23 +4,23 @@ const countStudents = require('./3-read_file_async');
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    handleStudentsRequest(req, res);
+    let dbInfo = 'This is the list of our students\n';
+    await countStudents(process.argv[2])
+      .then((msg) => {
+        dbInfo += msg;
+        res.end(dbInfo);
+      })
+      .catch((err) => {
+        dbInfo += err.message;
+        res.end(dbInfo);
+      });
   }
 });
-
-async function handleStudentsRequest(req, res) {
-  try {
-    const dbInfo = await countStudents(process.argv[2]);
-    res.end(`This is the list of our students\n${dbInfo}`);
-  } catch (err) {
-    res.end(`Error: ${err.message}`);
-  }
-}
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}`);
