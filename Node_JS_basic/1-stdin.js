557dd9956a93a7b49671b66da34973b3fef9ae42
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-05-31 13:33:04 -0400

    Coding

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,8 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (name) => {
+  process.stdout.write(`Your name is: ${name}`);
+});
 
-process.stdin.resume();
-
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim(); // Convertimos el búfer a cadena y eliminamos los espacios en blanco alrededor
-  if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
-  }
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
